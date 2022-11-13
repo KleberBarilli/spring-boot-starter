7913d7ef033dea209b983c44f8c999ec645135e9
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-13 11:53:29 -0300

    lombok getter e setters

--- src/main/java/br/com/starter/controllers/UserController.java
+++ src/main/java/br/com/starter/controllers/UserController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.starter.model.User;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello, world!";
+    public User user() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("Kleber");
+        user.setEmail("Kb@gmail.com");
+        return user;
     }
 
 }
