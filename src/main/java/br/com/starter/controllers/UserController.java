cfb110c9e13ad1dadb3e4f5780ed3f22cbbc720b
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-13 12:03:22 -0300

    wip

--- src/main/java/br/com/starter/controllers/UserController.java
+++ src/main/java/br/com/starter/controllers/UserController.java
@@ -1,6 +1,13 @@
 package br.com.starter.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +17,26 @@ import br.com.starter.model.User;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/")
-    public User user() {
-        User user = new User();
-        user.setId(1L);
-        user.setName("Kleber");
-        user.setEmail("Kb@gmail.com");
+    private List<User> users = new ArrayList<User>();
+
+    @GetMapping("/{id}")
+    public User user(@PathVariable("id") Long id) {
+        Optional<User> userFound = users.stream().filter(user -> user.getId() == id).findFirst();
+        if (userFound.isPresent()) {
+            return userFound.get();
+        }
+        return null;
+    }
+
+    @PostMapping("/")
+    public User user(@RequestBody User user) {
+        users.add(user);
         return user;
+    }
+
+    @GetMapping("/list")
+    public List<User> list() {
+        return users;
     }
 
 }
