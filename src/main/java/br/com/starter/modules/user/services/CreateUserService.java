5fad4cd370174f630a43c6d17d6b7ed2508247dc
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-19 00:51:02 -0300

    add bcrypt encoder

--- src/main/java/br/com/starter/modules/user/services/CreateUserService.java
+++ src/main/java/br/com/starter/modules/user/services/CreateUserService.java
@@ -1,6 +1,7 @@
 package br.com.starter.modules.user.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.starter.modules.user.repository.UserRepository;
@@ -12,6 +13,10 @@ public class CreateUserService {
     @Autowired
     UserRepository userRepository;
 
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    };
+
     public User execute(User user) {
 
         User existsUser = userRepository.findByName(user.getName());
@@ -19,6 +24,7 @@ public class CreateUserService {
         if (existsUser != null) {
             throw new Error("User already exists!");
         }
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
 
         User createdUser = userRepository.save(user);
 
